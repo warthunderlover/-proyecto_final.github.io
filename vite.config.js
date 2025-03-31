d12e6a758219d154a0b37e740451b25872e16e2f
warthunderlover <lazoemanuel29@gmail.com>
2025-03-30 23:23:04 -0600

    faq emanuel

---
+++ vite.config.js
@@ -0,0 +1,41 @@
+import path, { resolve } from "node:path";
+import { defineConfig } from 'vite';
+import * as glob from 'glob';
+import htmlPurge from 'vite-plugin-purgecss';
+import handlebars from 'vite-plugin-handlebars';
+//import {getpagecontext} from "./data";
+
+const obtenerEntradasHTML = ()=>{
+    return Object.fromEntries(
+        [
+            ...glob.sync('./**/*.html', {ignore: ["./dist/**", "./node_modules/**"]}
+
+            ).map (
+                fileData => [
+                    fileData.slice(0, fileData.length - path.extname(fileData).length),
+                    resolve(__dirname, fileData)
+                ]
+            )
+        ]
+    );
+}
+
+
+export default defineConfig({
+    appType: 'mpa',
+    base: process.env.DEPLOY_BASE_URL,
+    build: {
+        rollupOptions: {
+            input: obtenerEntradasHTML()
+        }
+    },
+    plugins: [
+        handlebars({
+            partialDirectory : resolve(__dirname, 'partials'),
+            //context: getpagecontext
+            
+        }),
+        htmlPurge({})
+    ]
+
+});
